9c7f99f6c472df96c7c98c6ea1cf59fe347ca8fe
Jibril Richardson <73800122+6ix0neJ@users.noreply.github.com>
2026-02-03 14:37:33 -0500

    Update index.js

--- In Class Projects/Simple Number Guesser/index.js
+++ In Class Projects/Simple Number Guesser/index.js
@@ -1,4 +1,18 @@
+//const prompt = require("prompt-sync")();
+const iWinURL = "https://image.pngaaa.com/329/1226329-middle.png";
+const iLoseURL = "https://www.vhv.rs/dpng/d/456-4569494_stick-people-png-win-win-vs-win-loose.png";
+
 function numGuess() {
+    const wImg = document.getElementById("iWin");
+    const lImg = document.getElementById("iLose");
+
+    // Set image sources
+    wImg.src = iWinURL;
+    lImg.src = iLoseURL;
+
+    // Hide both images initially
+    wImg.style.display = "none";
+    lImg.style.display = "none";
     let rNum = Math.floor(Math.random() * 10) + 1;
     let uNum = Number(document.getElementById("userGuess").value);
 
@@ -14,10 +28,16 @@ function numGuess() {
     if (uNum == rNum) {
         console.log("You win!");
         resultHTML += "You win!<br>";
+        wImg.style.display = "block";
+        wImg.style.top = "350px";
+        wImg.style.left = "8px";
         alert("You win!");
     } else {
         console.log("You lose! The number was: " + rNum);
         resultHTML += "You lose!<br>";
+        lImg.style.display = "block";
+        lImg.style.top = "350px";
+        lImg.style.left = "8px";
     }
 
     resultHTML += "The Number was: " + rNum + "<br>";
@@ -26,3 +46,4 @@ function numGuess() {
     document.getElementById("results").innerHTML = resultHTML;
 }
 numGuess();
+//alert("Game over!");
