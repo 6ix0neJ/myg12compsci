b8b35ede7aff2f0b2757c4e28b0f98fae1447d24
Jibril Richardson <73800122+6ix0neJ@users.noreply.github.com>
2026-02-03 14:38:45 -0500

    Add image display functionality to imageDisplay.js

---
+++ In Class Projects/Simple Number Guesser/imageDisplay.js
@@ -0,0 +1,7 @@
+imageURL = "https://bluemoji.io/cdn-proxy/646218c67da47160c64a84d5/66b3e5d0c2ab246786ca1d5e_86.png";
+const oImg = document.getElementById("bluethumbs");
+oImg.src = imageURL;
+
+oImg.style.top = "70px";
+oImg.style.left = "380px";
+oImg.style.position = "absolute";
