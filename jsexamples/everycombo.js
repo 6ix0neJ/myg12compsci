c79baee379f011c9de8240307115ff8a4bd358c0
Jibril Richardson <73800122+6ix0neJ@users.noreply.github.com>
2026-04-17 14:09:59 -0400

    Add permutation function to generate combinations

---
+++ jsexamples/everycombo.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+permute(arr, arr.length);
+function permute (arr,r, index = 0) {
+    if(index == r - 1) {
+        console.log(arr)
+        return
+    }
+    for (let i = index; i < r; i++) {
+        [arr[index],arr[i]] = [arr[i], arr[index]]
+        permute(arr, r, index + 1)
+    }
+}
